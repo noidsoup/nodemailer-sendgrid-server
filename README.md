d76b667220e5f4154b68181cc90af2f15c2e2683
noidsoup <nicholas.eymann@gmail.com>
2018-12-31 21:02:50 -0800

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Copy the `production.json` file to a `default.json` file in /config folder and populate it with our sendgrid api key.
+# nodemailer-sendgrid-server
+> send an email via nodemailer/sendgrid and store the record in a mongo DB
+
+### Pre-req
+You must be running mongo DB locally on your machine in order for the application to run.
+
+### Setup
+```
+npm install
+npm start
+'''
+
+Create a .env file and populate with your keys.
+
+Example: 
+
+NODE_ENV=dev
+SERVER_PORT=3001
+SENDGRID_API_KEY=SG.##############################
+
+### Usage
+A demonstration of sending an email via sendgrid and nodemailer. Every email is saved to a local mongo DB, in a collection called "Email"
+
+In order to send an email,  
--- index.js
+++ index.js
@@ -1,21 +1,29 @@
+// loads evnironment variables defined in .env
 require("dotenv").config();
+
+// config to switch between development, staging, or production
 const config = require("./config");
+
 const express = require("express");
 const bodyParser = require('body-parser');
+// winston for logging
 const logger = require('./utils/logger');
 
+// establishes api routes
 const api = require("./routes/api/v1");
 
+// sets up the express app
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// set up mongoose connection
+// sets up mongoose connection
 const mongoose = require("mongoose");
 const {
   db: { host, port, name },
 } = config.dev;
 
+// your mongoDB must be running to use this application
 const mongoDB = process.env.MONGODB_URI || `mongodb://${host}:${port}/${name}`;
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useFindAndModify: false });
